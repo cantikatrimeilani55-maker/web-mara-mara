df261e4d72f801027b427c32f02a24ffe713bdbe
cantikatrimeilani55-maker <cantikatrimeilani55@gmail.com>
2025-10-30 11:07:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const tombolSapa = document.getElementById('sapaButton')
 tombolSapa.addEventListener('click',function(){
-  alert('Halo! Terimakasih sudah berkunjung');});
+  alert('Halo! Terimakasih sudah berkunjung');
+});
